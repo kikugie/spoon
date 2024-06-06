a2bbb6dab1e51e117e086e3bb9a10e0cf0774437
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-06-05 22:53:41 -0500

    added some important todos

--- src/main/kotlin/xd/arkosammy/monkeyconfig/settings/StringListSetting.kt
+++ src/main/kotlin/xd/arkosammy/monkeyconfig/settings/StringListSetting.kt
@@ -2,6 +2,7 @@ package xd.arkosammy.monkeyconfig.settings
 
 import xd.arkosammy.monkeyconfig.util.SettingIdentifier
 
+// TODO: Make this an abstract generic class to support lists of any of the primitive data types
 open class StringListSetting(settingIdentifier: SettingIdentifier, comment: String? = null, defaultValue: MutableList<String>, value: MutableList<String> = defaultValue) : ConfigSetting<MutableList<String>>(settingIdentifier, comment, value) {
 
     class Builder(id: SettingIdentifier, defaultValue: MutableList<String>) : ConfigSetting.Builder<MutableList<String>, StringListSetting>(id, defaultValue) {
