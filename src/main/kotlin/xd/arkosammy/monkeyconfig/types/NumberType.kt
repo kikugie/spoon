a90016db08572b0cd608461cca3623010107adf3
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-06-16 22:44:30 -0500

    Remove the "out" type projection from the NumberType class

--- src/main/kotlin/xd/arkosammy/monkeyconfig/types/NumberType.kt
+++ src/main/kotlin/xd/arkosammy/monkeyconfig/types/NumberType.kt
@@ -1,4 +1,4 @@
 package xd.arkosammy.monkeyconfig.types
 
 @JvmInline
-value class NumberType<out T : Number>(override val rawValue: T) : SerializableType<T>
+value class NumberType<T : Number>(override val rawValue: T) : SerializableType<T>
